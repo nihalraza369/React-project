fbfaadbe4fec5d3ab209d34d305c08b08da006f1
nihalraza369 <nihalraza369@gmail.com>
2025-05-12 23:08:24 +0500

    Typing Speed Test project

---
+++ Typing Speed Test project/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+})
